e9685cdc5936ad7c0ac1cbdcb1b19a8a7c34f203
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-09-02 08:36:55 +0800

    update Tugas-7 soal 2

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -52,3 +52,41 @@ sungokong.yell()
 
 var kodok = new Frog("buduk")
 kodok.jump()
+
+
+// soal 2
+class Clock{
+	constructor({template}, timer){
+		this._template = template
+		this._timer = timer
+	}
+
+	render(){
+		let date = new Date()
+
+		let hours = date.getHours()
+		if (hours < 10) hours = '0' + hours
+
+		let mins = date.getMinutes()
+		if (mins < 10) mins = '0' + mins
+
+		let secs = date.getSeconds()
+		if (secs < 10) secs = '0' + secs
+
+		let output = this._template.replace('h', hours).replace('m', mins).replace('s', secs)
+
+		console.log(output)
+	}
+
+	stop(){
+		clearInterval(this._timer)
+	}
+
+	start(){
+		this.render()
+		this._timer = setInterval(this.render.bind(this), 1000)
+	}
+}
+console.log("\nOutput soal 2")
+var clock = new Clock({template: 'h:m:s'})
+clock.start()
