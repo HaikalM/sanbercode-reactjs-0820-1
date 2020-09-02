36159a89878bc15e8524523652efd89ccb141a1e
Maulana Haikal <maulana.haikal.98@gmail.com>
2020-09-02 08:26:53 +0800

    update Tugas-7 soal 1 release 1

--- Tugas-7/tugas7.js
+++ Tugas-7/tugas7.js
@@ -23,3 +23,32 @@ console.log("Output soal 1 release - 0")
 console.log("Sheep name : " + sheep.name)
 console.log("Sheep legs : " + sheep.legs)
 console.log("Sheep Cold Blooded : " + sheep.cold_blooded)
+
+// soal 1 -> release 1
+class Frog extends Animal{
+	constructor(name){
+		super(name)
+	}
+
+	jump(){
+		console.log("Frog jump : hop hop")
+	}
+}
+
+class Ape extends Animal{
+	constructor(name){
+		super(name)
+		this._legs = 2
+	}
+
+	yell() {
+		console.log("Ape yell : Auoo")
+	}
+}
+console.log("\nOutput soal 1 release - 1")
+
+var sungokong = new Ape("kera sakti")
+sungokong.yell()
+
+var kodok = new Frog("buduk")
+kodok.jump()
